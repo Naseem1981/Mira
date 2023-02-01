eae2f22a3bfe0a9436cb9481e39fbbc366a93827
Naseem1981 <naseemterblanche@gmail.com>
2023-02-01 15:20:57 +0200

    update new prompt

--- server/server.js
+++ server/server.js
@@ -27,9 +27,9 @@ app.post("/", async (req, res) => {
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: ` ${prompt}`,
+      prompt: `${prompt} can you conduct market research, competitor analysis, industry trends, potential revenue analysis, Can you write a comprehensive validation and provide some ideas to make it successfull`,
       temperature: 0.7, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      max_tokens: 2048, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
       frequency_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
       presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
